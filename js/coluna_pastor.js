fdc5b2c9dd53cc3577a5872dea870d308559847f
IBBVSCarlos <ibbvsaocarlos@gmail.com>
2025-05-20 22:56:33 -0300

    Add files via upload

---
+++ js/coluna_pastor.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("dados/coluna_pastor.json")
+    .then(res => res.json())
+    .then(data => {
+      const titulo = document.querySelector("#coluna-pastor h2");
+      const artigo = document.getElementById("mensagem-pastor");
+      titulo.textContent = data.titulo;
+      data.mensagem.forEach(paragrafo => {
+        const p = document.createElement("p");
+        p.textContent = paragrafo;
+        artigo.appendChild(p);
+      });
+    })
+    .catch(error => {
+      console.error("Erro ao carregar a Coluna do Pastor:", error);
+    });
+});
