63092ca2d2243b48b6900f664baa7bffb17c4ee2
IBBVSCarlos <ibbvsaocarlos@gmail.com>
2025-06-01 17:22:18 -0300

    update serviceWorker at script.js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // script.js
 // script para ativar o PWA
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js")
+  navigator.serviceWorker.register("service-worker.js")
     .then(() => console.log("✅ Service Worker registrado com sucesso!"))
     .catch((error) => console.log("⚠️ Erro ao registrar Service Worker:", error));
 }
