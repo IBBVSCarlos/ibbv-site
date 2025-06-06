5d20f1614bb2d196d73399d9512eec4bc3413f8f
IBBVSCarlos <ibbvsaocarlos@gmail.com>
2025-06-06 00:05:20 -0300

    ajustes refinados

--- assets/script.js
+++ assets/script.js
@@ -47,7 +47,7 @@ function definirSemanaReferencia() {
   const formatoData = data =>
     data.toLocaleDateString("pt-BR", { day: "2-digit", month: "long" });
 
-  const semanaTexto = `Semana: ${formatoData(inicioSemana)} a ${formatoData(fimSemana)}`;
+  const semanaTexto = ${formatoData(inicioSemana)} a ${formatoData(fimSemana)}`;
 
   // Atualizar conteúdo somente se o elemento existir
   const semanaReferenciaElemento = document.getElementById("semana-referencia");
